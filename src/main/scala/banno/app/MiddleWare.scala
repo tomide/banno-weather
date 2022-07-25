9db805eebf66b7db4ea4146624fbdd4a475f3369
tomide <ayotomideadedeji@gmail.com>
2022-07-25 11:29:16 -0400

    removed unUsed code

--- src/main/scala/banno/app/MiddleWare.scala
+++ src/main/scala/banno/app/MiddleWare.scala
@@ -1,21 +1,23 @@
 package banno.app
 
 import banno.app.errors.{AppErrorHandler, ErrorTypeToHttpStatus}
+import cats.data.Kleisli
 import cats.effect.{Clock, Sync}
 import com.olegpy.meow.hierarchy.deriveMonadErrorFromThrowable
 import io.chrisdavenport.log4cats.Logger
-import org.http4s.HttpRoutes
+import org.http4s.{HttpRoutes, Request, Response}
 import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
-import com.olegpy.meow.hierarchy._
 
 object MiddleWare {
 
-  def apply[F[_]: Sync: Logger: Clock](routes: HttpRoutes[F]) = {
+  def apply[F[_]: Sync: Logger: Clock](routes: HttpRoutes[F]): Kleisli[F, Request[F], Response[F]] = {
     implicit val logger: String => F[Unit] = (info: String) => {
       Logger[F].info(info)
     }
 
     val middleware: HttpRoutes[F] => HttpRoutes[F] = {
+      import com.olegpy.meow.hierarchy._
+
       { http: HttpRoutes[F] =>
         val handler = AppErrorHandler.apply[F](ErrorTypeToHttpStatus.default())
         handler(http)
--- src/main/scala/banno/app/errors/ErrorDetails.scala
+++ src/main/scala/banno/app/errors/ErrorDetails.scala
@@ -1,46 +1,19 @@
 package banno.app.errors
 
 import cats.Show
-import cats.data.NonEmptyList
-import cats.syntax.show._
-import io.circe.generic.auto.exportEncoder
-import io.circe.syntax._
 import io.circe.{Encoder, Json}
 
 sealed trait ErrorDetails
 
 object ErrorDetails {
-  case class Simple(msg: String)                               extends ErrorDetails
-  case class Complex(details: NonEmptyList[ErrorDetails])      extends ErrorDetails
-  case class WithSource(source: Source, details: ErrorDetails) extends ErrorDetails
-
-  case class Source(name: String, kind: String)
+  case class Simple(msg: String) extends ErrorDetails
 
   implicit val encoder: Encoder[ErrorDetails] = {
     case Simple(msg) =>
       Json.fromString(msg)
-
-    case WithSource(Source(name, kind), details) =>
-      Json.obj(
-        ("source", Json.fromString(name)),
-        ("kind", Json.fromString(kind)),
-        ("details", encoder.apply(details)),
-      )
-
-    case Complex(details) =>
-      details.toList.asJson
   }
 
   implicit val showInstance: Show[ErrorDetails] = {
     case Simple(msg) => msg
-
-    case WithSource(Source(name, kind), details) =>
-      show"""
-            | source = $name
-            | kind = $kind
-            | details = ${showInstance.show(details)}
-            |""".stripMargin
-
-    case Complex(details) => details.toList.map(showInstance.show).mkString("\n---")
   }
 }
